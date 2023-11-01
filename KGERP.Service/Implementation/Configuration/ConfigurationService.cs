04bd83650000c173c5ddfb0f6b1fec406a9dae62
borhanuddinamin <borhanuddinamin@gmail.com>
2023-11-01 17:46:45 +0600

    deport, dealer report done

--- KGERP.Service/Implementation/Configuration/ConfigurationService.cs
+++ KGERP.Service/Implementation/Configuration/ConfigurationService.cs
@@ -2076,7 +2076,9 @@ namespace KGERP.Service.Implementation.Configuration
 
             if (zoneId.HasValue && zoneId > 0 && regionId > 0)
             {
-                var v = _db.Areas.Where(x => x.CompanyId == companyId && x.ZoneId == zoneId && x.RegionId == regionId && x.IsActive == true).ToList()
+                var v = _db.Areas.Where(x => x.CompanyId == companyId && x.ZoneId == zoneId && x.RegionId == regionId 
+                
+                ).ToList()
                     .Select(x => new SelectModel()
                     {
                         Text = x.Name,
