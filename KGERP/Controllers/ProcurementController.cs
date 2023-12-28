c0c9ae65cf40d5aa7f5a740255df194e062cf50d
chittoranjan <chittoranjan.sarker@gmail.com>
2023-12-28 22:00:07 +0600

    dpt to dlr and sr sales page updated

--- KGERP/Controllers/ProcurementController.cs
+++ KGERP/Controllers/ProcurementController.cs
@@ -2248,7 +2248,7 @@ namespace KGERP.Controllers
             vmSalesOrderSlave.ZoneList = new SelectList(_service.ZonesDropDownList(companyId), "Value", "Text");
             vmSalesOrderSlave.CommonSupplier = new VMCommonSupplier();
             vmSalesOrderSlave.CommonSupplier.PaymentTypeList = new SelectList(_Configurationservice.CommonCustomerPaymentType(), "Value", "Text");
-           
+            vmSalesOrderSlave.StockInfoTypeId = (int)StockInfoTypeEnum.Dealer;
 
             return View(vmSalesOrderSlave);
         }
@@ -2414,6 +2414,8 @@ namespace KGERP.Controllers
             vmSalesOrderSlave.CommonSupplier.PaymentType = "Special";
             vmSalesOrderSlave.CommonSupplier.CustomerTypeFk = (int)CustomerType.Retail;
             vmSalesOrderSlave.CommonSupplier.CustomerStatus = (int)CustomerStatusEnum.CashCustomer;
+            vmSalesOrderSlave.StockInfoTypeId = (int)StockInfoTypeEnum.Dealer;
+
             return View(vmSalesOrderSlave);
         }
 
